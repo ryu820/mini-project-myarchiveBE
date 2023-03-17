0b522429e91d9a181c9ac5a45fad4d6641f5a4e7
song33ztpgg <sasasa7737@gmail.com>
2023-03-17 23:02:47 +0900

    song_update:3/17

--- app.js
+++ app.js
@@ -1,12 +1,12 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const usersRouter = require("./routes/user.route");
 const app = express();
 const PORT = 3017;
+app.use(express.json()); 
+app.use(cookieParser());
 
-app.use(cookieParser())
-app.use(express.json());
-
-
+app.use('/',[usersRouter]);
 
 app.listen(PORT, () => {
     console.log(` http://localhost:${PORT} `);
---
+++ routes/user.route.js
@@ -0,0 +1,64 @@
+const express = require('express');
+const jwt = require('jsonwebtoken');
+const router = express.Router();
+
+const { Users } = require('../models');
+
+const authMiddleware = require('../middlewares/auth-middleware');
+
+// ★회원가입
+router.post('/register', async (req, res) => {
+  try {
+    const { accountId, password, confirm, nick } = req.body;
+
+    
+    
+    
+    await Users.create({ accountId, password, nick });
+
+    return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ errorMessage: '회원가입에 실패하였습니다.' });
+  }
+});
+
+//★로그인
+router.post('/login', async (req, res) => {
+  try {
+    const { accountId, password } = req.body;
+
+    const user = await Users.findOne({ where: { accountId } });
+    if (!user) {
+      return res.json('존재하지 않는 닉네임입니다');
+    }
+
+    if (user.password != password) {
+      return res.json('비밀번호가 틀렸습니다');
+    }
+
+    //jwt생성
+    const token = jwt.sign({ userId: user.userId }, 'secret_key');
+
+    //쿠키발급
+    res.cookie('authorization', `Bearer ${token}`);
+  } catch (err) {
+    console.log(err);
+  }
+});
+
+//미들웨어 정보 가져오기
+router.get('/posts', authMiddleware, async (req, res) => {
+  try {
+    const { userId } = res.locals.user;
+
+    console.log(userId);
+
+    return res.status(201).json({ userId });
+  } catch (err) {
+    console.log(err);
+    return res.status(400).json({ userId });
+  }
+});
+
+module.exports = router;
