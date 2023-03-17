c7d3ac2926420777339cb1d4d937f07ac6e1583c
ryu820 <rhju820@naver.com>
2023-03-17 23:01:16 +0900

    mypage 사용자 조회 생성

---
+++ routes/mypage.route.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const authmiddleware = require("../middlewares/auth-middleware.js")
+const router = express.Router();
+
+router.get("/mypage", authmiddleware, async(req,res) => {
+    const account = req.locals.user;
+    const donepostlist = await Posts.findAll({
+        where : {
+            accountId : account,
+            isDone : true
+        }
+    })
+    const notDonepostlist = await Posts.findAll({
+        where : {
+            accountId : account,
+            isDone : false
+        }
+    })
+    const postslist = {done : donepostlist , notdone : notDonepostlist}
+    res.status(200).json({posts : postslist})
+})
+
+module.exports = router;
