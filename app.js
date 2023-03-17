e02a32bda2166a819e6c472ad4445d920934f69d
dabeenkim <ekqls6812@naver.com>
2023-03-17 23:02:36 +0900

    post 조회생성삭제api구현

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require("express");
 const cookieParser = require("cookie-parser");
+const postsRouter = require("./routes/posts.route");
+
 const app = express();
 const PORT = 3017;
 
 app.use(cookieParser())
 app.use(express.json());
+app.use("/list", postsRouter);
 
 app.listen(PORT, () => {
     console.log(` http://localhost:${PORT} `);
