a2cac6f1282e85e27bf47dd8fc0cb00223f17056
dabeenkim <ekqls6812@naver.com>
2023-03-18 09:09:10 +0900

    중간점검

--- app.js
+++ app.js
@@ -1,19 +1,17 @@
-const express = require("express");
-const cookieParser = require("cookie-parser");
-const postsRouter = require("./routes/posts.route");
-const usersRouter = require("./routes/user.route");
+const express = require('express');
+const cookieParser = require('cookie-parser');
+const postsRouter = require('./routes/posts.route');
+const usersRouter = require('./routes/user.route');
 
 const app = express();
 const PORT = 3017;
-app.use(express.json()); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
-
-app.use("/list", postsRouter);
-app.use('/',[usersRouter]);
-
+app.use('/list', postsRouter);
+app.use('/', [usersRouter]);
 
 app.listen(PORT, () => {
-    console.log(` http://localhost:${PORT} `);
-})
-
+  console.log(` http://localhost:${PORT} `);
+});
