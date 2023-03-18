6caa2ef44b1369204aa064479fcb090106388eec
dabeenkim <ekqls6812@naver.com>
2023-03-18 18:19:18 +0900

    다다다시

--- app.js
+++ app.js
@@ -5,9 +5,10 @@ const postsRouter = require("./routes/posts.route");
 const usersRouter = require("./routes/user.route");
 const MypageRouter = require("./routes/mypage.route");
 
-app.use(cors());
 const app = express();
 const PORT = 3017;
+
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
