c6f199c468fe95bb11834c0194a0367c3da63625
song33ztpgg <sasasa7737@gmail.com>
2023-03-18 15:08:10 +0900

    pull실수

--- routes/user.route.js
+++ routes/user.route.js
@@ -3,89 +3,170 @@ const jwt = require('jsonwebtoken');
 const router = express.Router();
 const { Users } = require('../models');
 
+require("dotenv").config();
+const env = process.env
 
-//★ 회원가입 아이디 중복확인
-router.post("/register/check-id", async (req,res) => {
-  const { accountId } = req.body; 
+
+/**
+ * ★ 회원가입 아이디 중복확인
+ */
+router.post('/register/check-id', async (req, res) => {
+  const { accountId } = req.body;
   const checkId = await Users.findOne({ where: { accountId } });
 
-  if(checkId){ 
-    return res.status(400).json({data : "중복된 아이디 입니다"});
+  if (checkId) {
+    return res
+      .status(412)
+      .json({ errorMessage: '중복된 아이디입니다.' });
   }
 
-  res.status(200).json({data: "사용가능한 아이디 입니다"});
-}); 
-
-//★ 회원가입 닉네임 중복확인
-router.post("/register/check-nick",async (req,res) => {
-  const { nick } = req.body;
-  const checkNick = await Users.findOne({ where: { nick } });
-  
-  if(checkNick){ 
-    return res.status(400).json({data : "중복된 닉네임 입니다"});
-  }
-  res.status(200).json({data: "사용가능한 닉네임 입니다"});
+  return res
+    .status(200)
+    .json({ message: '사용가능한 아이디 입니다' });
 });
 
-const regex = /^[a-zA-Z0-9]/;
+/**
+ * ★ 회원가입 닉네임 중복확인
+ */
+router.post('/register/check-nick', async (req, res) => {
+  const { nick } = req.body;
+  const checkNick = await Users.findOne({ where: { nick } });
 
+  if (checkNick) {
+    return res
+      .status(412)
+      .json({ errorMessage: '중복된 닉네임입니다.' });
+  }
+  return res
+    .status(200)
+    .json({ message: '사용가능한 닉네임 입니다' });
+});
 
-
-// ★회원가입
+/**
+ * ★회원가입
+ */
 router.post('/register', async (req, res) => {
   try {
     const { accountId, password, confirm, nick } = req.body;
+    const regex = /^[a-z0-9]{4,}$/; //정규 표현식: 조건 알파벳과숫자로 이루어진 4글자 이상
 
-    if(!regex.test(accountId)){ 
-      console.log(accountId + " = 형식이 일치하지 않습니다");
-      res.status(412).json({"errorMessage": "닉네임의 형식이 일치하지 않습니다."});
-    } else { 
-      console.log(accountId + " = 형식이 일치");
+
+    //닉네임 체크
+    const checkNick = await Users.findOne({ where: { nick } });
+    if (checkNick) {
+      return res
+        .status(400)
+        .json({ errorMessage: '중복된 닉네임입니다.' });
     }
-    
-    // # 412 password가 일치하지 않는 경우
-    // {"errorMessage": "패스워드가 일치하지 않습니다."}
-    // # 412 password 형식이 비정상적인 경우
-    // {"errorMessage": "패스워드 형식이 일치하지 않습니다.}
-    // # 412 password에 닉네임이 포함되어있는 경우
-    // {"errorMessage": "패스워드에 닉네임이 포함되어 있습니다."}
-    // # 412 닉네임이 중복된 경우
-    // {"errorMessage": "중복된 닉네임입니다."}
-    // # 400 예외 케이스에서 처리하지 못한 에러
-    // {"errorMessage": "요청한 데이터 형식이 올바르지 않습니다."}
-    
-    return res.send();
-    // await Users.create({ accountId, password, nick });
-    // return res.status(201).json({ message: '회원가입이 완료되었습니다.' });
+
+    if (!regex.test(nick)) {
+      console.log(nick + ' = 형식이 일치하지 않습니다');
+      return res
+        .status(412)
+        .json({ errorMessage: '닉네임의 형식이 일치하지 않습니다.' });
+    }
+
+
+    //아이디 체크
+    const checkId = await Users.findOne({ where: { accountId } });
+    if (checkId) {
+      return res
+        .status(400)
+        .json({ errorMessage: '중복된 아이디입니다.' });
+    }
+
+    if (!regex.test(accountId)) {
+      console.log(accountId + ' = 형식이 일치하지 않습니다');
+      return res
+        .status(412)
+        .json({ errorMessage: '닉네임의 형식이 일치하지 않습니다.' });
+    }
+
+
+    //비밀번호 체크
+    if (password != confirm) {
+      return res
+        .status(412)
+        .json({ errorMessage: '패스워드가 일치하지 않습니다.' });
+    }
+
+    if (!regex.test(password)) {
+      console.log(password + ' = 형식이 일치하지 않습니다');
+      return res
+        .status(412)
+        .json({ errorMessage: '패스워드 형식이 일치하지 않습니다.' });
+    }
+
+    if (password.toString().includes(nick)) {
+      return res
+        .status(412)
+        .json({ errorMessage: '패스워드에 닉네임이 포함되어 있습니다.' });
+    }
+
+    await Users.create({ accountId, password, nick });
+
+    return res.status(201).json({ message: '회원가입에 성공하였습니다' });
   } catch (err) {
     console.log(err);
-    return res.status(400).json({ errorMessage: '회원가입에 요청한 데이터 형식이 올바르지 않습니다..' });
+    return res
+      .status(400)
+      .json({ errorMessage: '요청한 데이터 형식이 올바르지 않습니다.' });
   }
 });
-
-//★로그인
+function test() {}
+/**
+ * ★로그인
+ */
 router.post('/login', async (req, res) => {
   try {
     const { accountId, password } = req.body;
 
+    // # 412 닉네임 또는 비밀번호 형식이 비정상적인 경우
+    // {"errorMessage": "닉네임 또는 비밀번호의 형식이 비정상적입니다."}
+
     const user = await Users.findOne({ where: { accountId } });
     if (!user) {
-      return res.json('닉네임 또는 패스워드를 확인 해주세요(닉네임)');
+      return res
+        .status(401)
+        .json({ errorMessage: '존재하지 않는 유저입니다.' });
     }
 
     if (user.password != password) {
-      return res.json('닉네임 또는 패스워드를 확인 해주세요(비밀번호)');
+      return res
+        .status(412)
+        .json({ errorMessage: '비밀번호가 일치하지 않습니다.' });
     }
 
     //jwt생성
-    const token = jwt.sign({ userId: user.userId }, 'secret_key');
+    const token = jwt.sign({ userId: user.userId }, env.SECRET_KEY);
 
     //쿠키발급
     res.cookie('authorization', `Bearer ${token}`);
+    res.status(200).json({ token });
   } catch (err) {
     console.log(err);
+    res.status(400).json({ errorMessage: '로그인에 실패하였습니다.' });
   }
 });
 
+//★마스터 로그인
+const authMiddleware = require('../middlewares/auth-middleware');
+router.get('/master', authMiddleware, async (req, res) => {
+  const { accountId } = res.locals.user;
+
+  if (accountId != 'master') {
+    return res.send('권한이 없습니다');
+  }
+
+  let user = await Users.findAll({});
+
+  user = user.map((value) => {
+    return {
+      accountId: value.accountId,
+    };
+  });
+
+  return res.json(user);
+});
 
 module.exports = router;
