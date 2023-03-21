2cff97a4dadd7d5781b7a75e1315d33187ae020a
dabeenkim <ekqls6812@naver.com>
2023-03-21 13:05:22 +0900

    [feat]아키텍쳐구현

--- repositories/detail.repositories.js
+++ repositories/detail.repositories.js
@@ -1,67 +1,68 @@
-// const { Posts, Users, Comments } = require("../models");
+const CustomError = require("../middlewares/errorhandler");
+const { Posts, Users, Comments } = require("../models");
 
-// class DetailRepository extends Comments {
-//   constructor() {
-//     super();
-//   }
+class DetailRepository extends Comments {
+  constructor() {
+    super();
+  }
 
-//   OnesPost = async (postId) => {
-//     const detail_posts = await Posts.findOne({
-//       where: { postId },
-//       attributes: [
-//         "postId",
-//         "User.accountId",
-//         "User.nick",
-//         "url",
-//         "img",
-//         "category",
-//         "title",
-//         "desc",
-//         "isDone",
-//       ],
-//       raw: true,
-//       include: [
-//         {
-//           model: Users,
-//           attributes: [],
-//         },
-//       ],
-//     });
-//     return detail_posts;
-//   };
+  OnesPost = async (postId) => {
+    const detail_posts = await Posts.findOne({
+      where: { postId },
+      attributes: [
+        "postId",
+        "User.accountId",
+        "User.nick",
+        "url",
+        "img",
+        "category",
+        "title",
+        "desc",
+        "isDone",
+      ],
+      raw: true,
+      include: [
+        {
+          model: Users,
+          attributes: [],
+        },
+      ],
+    });
+    return detail_posts;
+  };
 
-//   getComment = async (postId) => {
-//     const comments = await Comments.findAll({
-//       where: { postId },
-//       attributes: ["commentId", "postId", "User.nick", "comment"],
-//       raw: true,
-//       include: [
-//         {
-//           model: Users,
-//           attributes: [],
-//         },
-//       ],
-//     });
-//     return comments;
-//   };
+  getComment = async (postId) => {
+    const comments = await Comments.findAll({
+      where: { postId },
+      attributes: ["commentId", "postId", "User.nick", "comment"],
+      raw: true,
+      include: [
+        {
+          model: Users,
+          attributes: [],
+        },
+      ],
+    });
+    return comments;
+  };
 
-//   postComment = async (postId, userId, comment) => {
-//     const comments = await Comments.create({
-//       userId: userId,
-//       postId: postId,
-//       comment: comment,
-//     });
-//     return comments;
-//   };
+  postComment = async (postId, userId, comment) => {
+    const comments = await Comments.create({
+      userId: userId,
+      postId: postId,
+      comment: comment,
+    });
+    return comments;
+  };
 
-//   delComment = async (postId, userId, commentId, comment) => {
-//     comment = await Comments.destroy({
-//       where: {
-//         [Op.and]: [{ postId }, { userId }, { commentId }],
-//       },
-//     });
-//     return comment;
-//   };
-// }
+  delComment = async (postId, userId, commentId, comment) => {
+    comment = await Comments.destroy({
+      where: {
+        [Op.and]: [{ postId }, { userId }, { commentId }],
+      },
+    });
+    return comment;
+  };
+}
 
-// module.exports = DetailRepository;
+module.exports = DetailRepository;
--- routes/detail.route.js
+++ routes/detail.route.js
@@ -5,138 +5,138 @@ const router = express.Router();
 const { Posts, Comments, Users } = require("../models");
 const { Op } = require("sequelize");
 
-// const DetailController = require("../controllers/detail.controllers");
-// const detailsController = new DetailController();
+const DetailController = require("../controllers/detail.controllers");
+const detailsController = new DetailController();
 
-// router.get("/:postId", detailsController.OnesPost);
-// router.get("/:postId/comments", detailsController.getComment);
-// router.post("/:postId/comments", authmiddleware, detailsController.postComment);
-// router.delete(
-//   "/:postId/comments/:commentId",
-//   authmiddleware,
-//   detailsController.delComment
-// );
-
-//게시물 단일조회
-//localhost:3017/post/:postId
-router.get("/:postId", async (req, res, next) => {
-  const { postId } = req.params;
-  try {
-    const detail_posts = await Posts.findOne({
-      where: { postId },
-      attributes: [
-        "postId",
-        "User.accountId",
-        "User.nick",
-        "url",
-        "img",
-        "category",
-        "title",
-        "desc",
-        "isDone",
-      ],
-      raw: true,
-      include: [
-        {
-          model: Users,
-          attributes: [],
-        },
-      ],
-    });
-    res.status(200).json({ detail: detail_posts });
-  } catch (error) {
-    next(error);
-    res.status(400).json({ errorMessage: "게시글 조회에 실패하였습니다." });
-  }
-});
-
-//게시물 별 댓글조회
-//localhost:3017/post/:postId/comments
-router.get("/:postId/comments", async (req, res, next) => {
-  try {
-    const { postId } = req.params;
-    const comments = await Comments.findAll({
-      where: { postId },
-      attributes: ["commentId", "postId", "User.nick", "comment"],
-      raw: true,
-      include: [
-        {
-          model: Users,
-          attributes: [],
-        },
-      ],
-    });
-    if (!comments) {
-      throw new CustomError("게시글이 존재하지 않습니다.", 404);
-    }
-    res.status(200).json({ detail: comments });
-  } catch (error) {
-    next(error);
-    res.status(400).json({ errorMessage: "댓글 조회에 실패하였습니다." });
-  }
-});
-
-//게시물 별 댓글 작성
-//localhost:3017/post/:postId/comments
-router.post("/:postId/comments", authmiddleware, async (req, res, next) => {
-  try {
-    const { userId } = res.locals.user;
-    const { postId } = req.params;
-    const { comment } = req.body;
-
-    if (!comment) {
-      throw new CustomError("comment를 입력해주세요", 410);
-    }
-    if (Number(comment.length > 100 || typeof comment !== "string")) {
-      throw new CustomError("글자 수를 초과하였습니다.", 412);
-    }
-    const comments = await Comments.create({
-      userId: userId,
-      postId: postId,
-      comment: comment,
-    });
-
-    res.status(201).json({ comments, message: "댓글 작성에 성공하였습니다." });
-  } catch (error) {
-    next(error);
-    res.status(401).json({ errorMessage: "댓글 작성에 실패하였습니다." });
-  }
-});
-
-//댓글 삭제
-//localhost:3017/post/:postId/comments/:commentId
+router.get("/:postId", detailsController.OnesPost);
+router.get("/:postId/comments", detailsController.getComment);
+router.post("/:postId/comments", authmiddleware, detailsController.postComment);
 router.delete(
   "/:postId/comments/:commentId",
   authmiddleware,
-  async (req, res, next) => {
-    try {
-      const { postId, commentId } = req.params;
-      const { userId } = res.locals.user;
-
-      let comment = await Comments.findOne({ where: { commentId } });
-      console.log(comment);
-      if (!comment || comment === null) {
-        throw new CustomError("댓글이 존재하지 않습니다.", 404);
-      }
-      if (!postId) {
-        throw new CustomError("댓글이 존재하지 않습니다1.", 404);
-      } else if (comment.userId !== userId) {
-        throw new CustomError("댓글의 삭제권한이 존재하지 않습니다.", 403);
-      }
-      comment = await Comments.destroy({
-        where: {
-          [Op.and]: [{ postId }, { userId }, { commentId }],
-        },
-      });
-
-      return res.status(200).json({ message: "댓글 삭제에 성공하였습니다." });
-    } catch (error) {
-      next(error);
-      res
-        .status(401)
-        .json({ errorMessage: "댓글이 정상적으로 삭제되지 않았습니다." });
-    }
-  }
+  detailsController.delComment
 );
+
+//게시물 단일조회
+//localhost:3017/post/:postId
+// router.get("/:postId", async (req, res, next) => {
+//   const { postId } = req.params;
+//   try {
+//     const detail_posts = await Posts.findOne({
+//       where: { postId },
+//       attributes: [
+//         "postId",
+//         "User.accountId",
+//         "User.nick",
+//         "url",
+//         "img",
+//         "category",
+//         "title",
+//         "desc",
+//         "isDone",
+//       ],
+//       raw: true,
+//       include: [
+//         {
+//           model: Users,
+//           attributes: [],
+//         },
+//       ],
+//     });
+//     res.status(200).json({ detail: detail_posts });
+//   } catch (error) {
+//     next(error);
+//     res.status(400).json({ errorMessage: "게시글 조회에 실패하였습니다." });
+//   }
+// });
+
+// //게시물 별 댓글조회
+// //localhost:3017/post/:postId/comments
+// router.get("/:postId/comments", async (req, res, next) => {
+//   try {
+//     const { postId } = req.params;
+//     const comments = await Comments.findAll({
+//       where: { postId },
+//       attributes: ["commentId", "postId", "User.nick", "comment"],
+//       raw: true,
+//       include: [
+//         {
+//           model: Users,
+//           attributes: [],
+//         },
+//       ],
+//     });
+//     if (!comments) {
+//       throw new CustomError("게시글이 존재하지 않습니다.", 404);
+//     }
+//     res.status(200).json({ detail: comments });
+//   } catch (error) {
+//     next(error);
+//     res.status(400).json({ errorMessage: "댓글 조회에 실패하였습니다." });
+//   }
+// });
+
+// //게시물 별 댓글 작성
+// //localhost:3017/post/:postId/comments
+// router.post("/:postId/comments", authmiddleware, async (req, res, next) => {
+//   try {
+//     const { userId } = res.locals.user;
+//     const { postId } = req.params;
+//     const { comment } = req.body;
+
+//     if (!comment) {
+//       throw new CustomError("comment를 입력해주세요", 410);
+//     }
+//     if (Number(comment.length > 100 || typeof comment !== "string")) {
+//       throw new CustomError("글자 수를 초과하였습니다.", 412);
+//     }
+//     const comments = await Comments.create({
+//       userId: userId,
+//       postId: postId,
+//       comment: comment,
+//     });
+
+//     res.status(201).json({ comments, message: "댓글 작성에 성공하였습니다." });
+//   } catch (error) {
+//     next(error);
+//     res.status(401).json({ errorMessage: "댓글 작성에 실패하였습니다." });
+//   }
+// });
+
+// //댓글 삭제
+// //localhost:3017/post/:postId/comments/:commentId
+// router.delete(
+//   "/:postId/comments/:commentId",
+//   authmiddleware,
+//   async (req, res, next) => {
+//     try {
+//       const { postId, commentId } = req.params;
+//       const { userId } = res.locals.user;
+
+//       let comment = await Comments.findOne({ where: { commentId } });
+//       console.log(comment);
+//       if (!comment || comment === null) {
+//         throw new CustomError("댓글이 존재하지 않습니다.", 404);
+//       }
+//       if (!postId) {
+//         throw new CustomError("댓글이 존재하지 않습니다1.", 404);
+//       } else if (comment.userId !== userId) {
+//         throw new CustomError("댓글의 삭제권한이 존재하지 않습니다.", 403);
+//       }
+//       comment = await Comments.destroy({
+//         where: {
+//           [Op.and]: [{ postId }, { userId }, { commentId }],
+//         },
+//       });
+
+//       return res.status(200).json({ message: "댓글 삭제에 성공하였습니다." });
+//     } catch (error) {
+//       next(error);
+//       res
+//         .status(401)
+//         .json({ errorMessage: "댓글이 정상적으로 삭제되지 않았습니다." });
+//     }
+//   }
+// );
 
 module.exports = router;
