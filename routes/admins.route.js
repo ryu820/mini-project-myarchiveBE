090e62f55405a2460cd7901ec30066c8a675e847
ryu820 <rhju820@naver.com>
2023-03-21 13:06:22 +0900

    [fix]관리자페이지 유저삭제 업데이트

--- routes/admins.route.js
+++ routes/admins.route.js
@@ -4,17 +4,20 @@ const CustomError = require("../middlewares/errorhandler");
 const router = express.Router();
 
 //관리자 로그인 API
-//localhost:3017/admin
-router.post("/admin", async (req, res, next) => {
-    const { accountId, password, secretkey } = req.body;
+//localhost:3017/admin/login
+router.post("/admin/login", async (req, res, next) => {
+    const { accountId, password, secretKey } = req.body;
     try {
         const adminUser = await Admins.findOne({ where: { accountId } })
-        if (!adminUser || password !== adminUser.password) {
-            throw new CustomError("아이디나 비밀번호가 틀렸슈~", 412)
-        } else if (secretkey !== adminUser.secretkey) {
-            throw new CustomError("시크릿키가 틀렸슈~", 412)
+        if (!adminUser) {
+            throw new CustomError("아이디가 존재하지 않습니다.", 412)
+        } else if (password !== adminUser.password) {
+            throw new CustomError("비밀번호가 일치하지 않습니다.", 412)
+        } else if (secretKey !== adminUser.secretkey) {
+            throw new CustomError("시크릿키가 일치하지 않습니다.", 412)
         }
-        res.status(200).json({ "message": "로그인 성공했슈~" })
+        // console.log(adminUser.nick)
+        res.status(200).json({ "message": `${adminUser.nick}님이 로그인하였습니다.` })
     } catch (error) {
         next(error)
         // res.status(400).json({"errorMessage" : "뭔가 이상해유"})
@@ -23,20 +26,30 @@ router.post("/admin", async (req, res, next) => {
 
 
 //관리자:전체유저조회API
-//localhost:3017/admin/user
-router.get("/admin/user", async (req, res, next) => {
+//localhost:3017/admin/users
+router.get("/admin/users", async (req, res, next) => {
     try {
-        const posts = await Users.findAll()
-        res.status(200).json(posts)
-    }catch(error){
+        const users = await Users.findAll()
+        res.status(200).json({ "users": users })
+    } catch (error) {
         next(error)
     }
 })
 
 //관리자:특정유저삭제API
-// router.post("/admin/user", async (req, res, next) => {
+//localhost:3017/admin/users/:userId
+router.delete("/admin/users/:userId", async (req, res, next) => {
+    const { userId } = req.params;
+    try {
+        await Users.destroy({
+            where: { userId }
+        })
+        res.status(200).json({ "message": "해당 회원을 삭제하였습니다." })
+    } catch(error){
+        next(error)
+    }
 
-// })
+})
 
 
 
