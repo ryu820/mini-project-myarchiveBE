55fd22c0a4c495b39d62dea6d769b3a959243eee
dabeenkim <ekqls6812@naver.com>
2023-03-21 09:20:13 +0900

    [feat]아키텍쳐주석

--- controllers/detail.controllers.js
+++ controllers/detail.controllers.js
@@ -1,70 +1,70 @@
-const DetailService = require("../services/detail.services");
+// const DetailService = require("../services/detail.services");
 
-class DetailController {
-  constructor() {
-    this.DetailService = new DetailService();
-  }
+// class DetailController {
+//   constructor() {
+//     this.DetailService = new DetailService();
+//   }
 
-  OnesPost = async (req, res, next) => {
-    try {
-      const { postId } = req.params;
-      const detail_post = await this.DetailService.OnesPost(postId);
-      res.status(200).json({ detail: detail_post });
-    } catch (error) {
-      next(error);
-      res.status(400).json({ errorMessage: "게시글 조회에 실패하였습니다." });
-    }
-  };
+//   OnesPost = async (req, res, next) => {
+//     try {
+//       const { postId } = req.params;
+//       const detail_post = await this.DetailService.OnesPost(postId);
+//       res.status(200).json({ detail: detail_post });
+//     } catch (error) {
+//       next(error);
+//       res.status(400).json({ errorMessage: "게시글 조회에 실패하였습니다." });
+//     }
+//   };
 
-  getComment = async (req, res, next) => {
-    try {
-      const { postId } = req.params;
-      const comments = await this.DetailService.getComment(postId);
-      res.status(200).json({ detail: comments });
-    } catch (error) {
-      next(error);
-      res.status(400).json({ errorMessage: "댓글 조회에 실패하였습니다." });
-    }
-  };
+//   getComment = async (req, res, next) => {
+//     try {
+//       const { postId } = req.params;
+//       const comments = await this.DetailService.getComment(postId);
+//       res.status(200).json({ detail: comments });
+//     } catch (error) {
+//       next(error);
+//       res.status(400).json({ errorMessage: "댓글 조회에 실패하였습니다." });
+//     }
+//   };
 
-  postComment = async (req, res, next) => {
-    try {
-      const { userId } = res.locals.user;
-      const { postId } = req.params;
-      const { comment } = req.body;
+//   postComment = async (req, res, next) => {
+//     try {
+//       const { userId } = res.locals.user;
+//       const { postId } = req.params;
+//       const { comment } = req.body;
 
-      const createComment = await this.DetailService.postComment(
-        userId,
-        postId,
-        comment
-      );
-      res
-        .status(201)
-        .json({ createComment, message: "댓글 작성에 성공하였습니다." });
-    } catch (error) {
-      next(error);
-      res.status(401).json({ errorMessage: "댓글 작성에 실패하였습니다." });
-    }
-  };
+//       const createComment = await this.DetailService.postComment(
+//         userId,
+//         postId,
+//         comment
+//       );
+//       res
+//         .status(201)
+//         .json({ createComment, message: "댓글 작성에 성공하였습니다." });
+//     } catch (error) {
+//       next(error);
+//       res.status(401).json({ errorMessage: "댓글 작성에 실패하였습니다." });
+//     }
+//   };
 
-  delComment = async (req, res, next) => {
-    try {
-      const { postId, commentId } = req.params;
-      const { userId } = res.locals.user;
+//   delComment = async (req, res, next) => {
+//     try {
+//       const { postId, commentId } = req.params;
+//       const { userId } = res.locals.user;
 
-      const deleteComment = await this.DetailService.delComment(
-        postId,
-        commentId,
-        userId
-      );
-      res.status(200).json({ message: "댓글 삭제에 성공하였습니다." });
-    } catch (error) {
-      next(error);
-      res
-        .status(401)
-        .json({ errorMessage: "댓글이 정상적으로 삭제되지 않았습니다." });
-    }
-  };
-}
+//       const deleteComment = await this.DetailService.delComment(
+//         postId,
+//         commentId,
+//         userId
+//       );
+//       res.status(200).json({ message: "댓글 삭제에 성공하였습니다." });
+//     } catch (error) {
+//       next(error);
+//       res
+//         .status(401)
+//         .json({ errorMessage: "댓글이 정상적으로 삭제되지 않았습니다." });
+//     }
+//   };
+// }
 
-module.exports = DetailController;
+// module.exports = DetailController;
--- services/detail.services.js
+++ services/detail.services.js
@@ -1,51 +1,51 @@
-const DetailRepository = require("../repositories/detail.repositories");
+// const DetailRepository = require("../repositories/detail.repositories");
 
-class DetailService {
-  constructor() {
-    this.DetailRepository = new DetailRepository();
-  }
+// class DetailService {
+//   constructor() {
+//     this.DetailRepository = new DetailRepository();
+//   }
 
-  OnesPost = async (postId) => {
-    try {
-      const getPost = await this.DetailRepository.OnesPost(postId);
+//   OnesPost = async (postId) => {
+//     try {
+//       const getPost = await this.DetailRepository.OnesPost(postId);
 
-      return getPost;
-    } catch (error) {}
-  };
+//       return getPost;
+//     } catch (error) {}
+//   };
 
-  getComment = async (postId) => {
-    try {
-      const comment = await this.DetailRepository.getComment(postId);
+//   getComment = async (postId) => {
+//     try {
+//       const comment = await this.DetailRepository.getComment(postId);
 
-      return comment;
-    } catch (error) {}
-  };
+//       return comment;
+//     } catch (error) {}
+//   };
 
-  postComment = async (postId, userId, comment) => {
-    try {
-      const createComment = await this.DetailRepository.postComment(
-        postId,
-        userId,
-        comment
-      );
+//   postComment = async (postId, userId, comment) => {
+//     try {
+//       const createComment = await this.DetailRepository.postComment(
+//         postId,
+//         userId,
+//         comment
+//       );
 
-      return createComment;
-    } catch (error) {}
-  };
+//       return createComment;
+//     } catch (error) {}
+//   };
 
-  delComment = async (postId, userId, commentId) => {
-    try {
-      let comment = await Comments.findOne({ where: { commentId } });
-      const delcomment = await this.DetailRepository.delComment(
-        postId,
-        userId,
-        commentId,
-        comment
-      );
+//   delComment = async (postId, userId, commentId) => {
+//     try {
+//       let comment = await Comments.findOne({ where: { commentId } });
+//       const delcomment = await this.DetailRepository.delComment(
+//         postId,
+//         userId,
+//         commentId,
+//         comment
+//       );
 
-      return delcomment;
-    } catch (error) {}
-  };
-}
+//       return delcomment;
+//     } catch (error) {}
+//   };
+// }
 
-module.exports = DetailService;
+// module.exports = DetailService;
